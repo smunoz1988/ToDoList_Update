3ee8a2954aa37d2c69b606356b95916cd5aedc57
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-04-09 22:08:30 -0500

    Add render function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import './style.css';
 
-toDoTasks = [
+const toDoTasks = [
   { description: 'buy groceries', completed: false, index: 1},
-  { description: 'buy groceries', completed: false, index: 2},
-  { description: 'buy groceries', completed: false, index: 3}
+  { description: 'eat 2 apples', completed: false, index: 2},
+  { description: 'make 3 clean', completed: false, index: 3}
 ];
 
 
-const renderTask = () => {
-  taskList = document.getElementById('container');
+const renderTask = (list) => {
+  const taskList = document.getElementById('container');
   taskList.innerHtml = '';
-  toDoTasks.forEach((task) => {
+  list.forEach((task) => {
+    const newItem = itemTemplate(task);
+    taskList.innerHTML += newItem;
   }); 
 }
+
+const itemTemplate = (task) => {
+  const itemTemplate = `
+    <div class="item">
+      <input type="checkbox" class="itemCompleted" ${task.completed ? 'checked' : ''}>
+      <input type="text" class="itemDescription" value='${task.description}'>
+      <button class="clearBtn">Clear</button>
+    </div>
+    `;
+  return itemTemplate;
+}
+ 
+
+window.onload = renderTask(toDoTasks);
+
+console.log('test');
